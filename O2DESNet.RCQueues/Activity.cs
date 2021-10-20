3c5406885386e376ff337987c834cb1a84203a81
Carl Chung <iecarl.hk@gmail.com>
2021-10-20 15:31:48 +0800

    Implement activity conditions

--- O2DESNet.RCQueues/Activity.cs
+++ O2DESNet.RCQueues/Activity.cs
@@ -20,6 +20,13 @@ namespace O2DESNet.RCQueues
         /// Result, -1 means first parameter is processed first, 1 means first parameter last
         /// </summary>
         Func<(IBatch Batch, DateTime Time), (IBatch Batch, DateTime Time), int> BatchOrder { get; set; }
+        /// <summary>
+        /// Activity Conditions Dictionary.
+        /// The key is the load object property name.
+        /// The value is the matched load property value for that activity.
+        /// All conditions much be matched for processing that activity
+        /// </summary>
+        Dictionary<string, object> Conditions { get; set; }
     }
 
     /// <summary>
@@ -34,5 +41,6 @@ namespace O2DESNet.RCQueues
         public Func<Random, IEnumerable<ILoad>, IAllocation, TimeSpan> Duration { get; set; }
         public Func<Random, ILoad, IActivity> Succeedings { get; set; }
         public Func<(IBatch Batch, DateTime Time), (IBatch Batch, DateTime Time), int> BatchOrder { get; set; } = (t1, t2) => t1.Time.CompareTo(t2.Time);
+        public Dictionary<string, object> Conditions { get; set; } = new Dictionary<string, object>();
     }
 }
